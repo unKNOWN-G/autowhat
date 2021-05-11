5acf2fcc47cc2ca7b2bd9dd5d9b9d150796cd072
Giri <karnatisaivenkatagiri@gmail.com>
2021-05-11 11:35:59 +0530

    Constructor file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from autowhat.sender import document_sender as document_sender
+from autowhat.sender import image_sender as image_sender
+from autowhat.sender import audio_sender as audio_sender
+from autowhat.sender import text_sender as text_sender
+from autowhat.initializer import whatsapp_initializer as whatsapp_starter
+from autowhat.initializer import whatsapp_closer as whatsapp_closer
+from autowhat.spammer import spam_bot as spam_bot
+from autowhat.group_creator import group_creator as group_creator
